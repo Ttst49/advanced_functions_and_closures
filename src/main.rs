f2a2fd0c0d2957bf8ae21f230b23d7201b9f063d
Thibaut Stachnick <thibautstachnick@gmail.com>
2024-05-03 16:57:05 +0200

    add a way to use a function inside another function parameter

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+fn add_one(x:i64)->i64{
+    x+1
+}
+
+fn make_it_twice(f:fn(i64)->i64,arg:i64)->i64{
+    f(arg)+f(arg)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let response = make_it_twice(add_one,5);
+    println!("The response is {}",response)
 }
