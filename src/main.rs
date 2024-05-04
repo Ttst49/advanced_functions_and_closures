016825e60205870a9eec474aabcdbfdc5d50608a
Thibaut Stachnick <thibautstachnick@gmail.com>
2024-05-04 13:39:21 +0200

    add functions about closures and allow unused functions inside the file

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,31 @@
+#[allow(unused)]
 fn add_one(x:i64)->i64{
     x+1
 }
 
+#[allow(unused)]
 fn make_it_twice(f:fn(i64)->i64,arg:i64)->i64{
     f(arg)+f(arg)
 }
 
+#[allow(unused)]
+fn closure_or_function(){
+
+    let some_numbers = vec![1,2,3];
+    //using closures
+    let some_chains: Vec<String> =
+    some_numbers.iter().map(|i| i.to_string()).collect();
+    //using function
+    let some_chains:Vec<String> =
+    some_numbers.iter().map(ToString::to_string).collect();
+}
+
+#[allow(unused)]
+fn return_closure()//-> dyn Fn(i32) -> i32
+{
+    |x| x+1;
+}
+
 fn main() {
-    let response = make_it_twice(add_one,5);
-    println!("The response is {}",response)
+    return_closure()
 }
